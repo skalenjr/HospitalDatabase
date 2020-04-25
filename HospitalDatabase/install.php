1a9bf418e1af8d41c8f0a92fd5325e6f81bc0248
kalensn <kalensn@mymail.vcu.edu>
2020-04-25 00:39:45 -0400

    fixed innstall.php

--- HospitalDatabase/install.php
+++ HospitalDatabase/install.php
@@ -5,7 +5,7 @@ require_once('connection.php');
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = file_get_contents("database.sql");
+    $sql = file_get_contents("init.sql");
     $conn->exec($sql);
     
 } catch (PDOException $e) {
