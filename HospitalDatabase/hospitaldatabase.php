915da86fb798841b64b8c9bfa403a183e4d7aa99
kalensn <kalensn@mymail.vcu.edu>
2020-04-25 02:09:23 -0400

    added more pages

--- HospitalDatabase/hospitaldatabase.php
+++ HospitalDatabase/hospitaldatabase.php
@@ -6,20 +6,22 @@
 <h2>Patients</h2>
 <ul>
 <li><a href="Patients/patients.php">View all patients</a></li>
-<li><a href="Patients/addPatient.php">Add patient</a></li>
 <li><a href="Patients/patient.php">View a patient's information</a></li>
+<li><a href="Patients/addPatient.php">Add patient</a></li>
 <li><a href="Patients/editPatient.php">Edit a patient's information</a></li>
 </ul>
 <h2>Visits</h2>
 <ul>
 <li><a href="Visits/visits.php">View all visits</a></li>
+<li><a href="Visits/visit.php">View a specific visit</a></li>
 <li><a href="Visits/patientsVisits.php">View a patient's visits</a></li>
 <li><a href="Visits/addVisit.php">Input visit informantion</a></li>
 </ul>
 <h2>Procedures</h2>
 <ul>
-<li><a href="Procedures/patientsProcedures.php">View a patient's procedures</a></li>
+<li><a href="Visits/procedures.php">View all procedures</a></li>
 <li><a href="Procedures/procedure.php">View a specific procedure</a></li>
+<li><a href="Procedures/patientsProcedures.php">View a patient's procedures</a></li>
 <li><a href="Procedures/addProcedure.php">Input procedure information</a></li>
 </ul>
 <h2>Prescriptions</h2>
@@ -30,7 +32,7 @@
 <h2>Employees</h2>
 <ul>
 <li><a href="employees/employees.php">View employees</a></li>
-<li><a href="employees/employee.php">View employee information</a></li>
+<li><a href="employees/employee.php">View an employee's information</a></li>
 <li><a href="employees/addEmployee.php">Add an employee</a></li>
 <li><a href="employees/editEmployee.php">Edit an employee's information</a></li>
 </ul>
