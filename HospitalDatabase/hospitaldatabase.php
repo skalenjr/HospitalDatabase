4cca007b13daa50003f456b5d6e62756028ca68a
kalensn <kalensn@mymail.vcu.edu>
2020-04-24 23:00:51 -0400

    Actually added connection page
    Actually added hospitaldatabase as landing page after login

---
+++ HospitalDatabase/hospitaldatabase.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>Hospital Database</title>
+</head>
+<body>
+<h1>Patients</h1>
+<ul>
+<li><a href="patients.php">View all patients</a></li>
+<li><a href="addPatient.php">Add patient</a></li>
+<li><a href="patient.php">View patient's information</a></li>
+<li><a href="editPatient.php">Edit patient information</a></li>
+</ul>
+<h1>Visits</h1>
+<ul>
+<li><a href="visits.php">View all visits</a></li>
+<li><a href="patientsVisits.php">View visits for a patient</a></li>
+<li><a href="addVisit.php">Input Visit informantion</a></li>
+</ul>
+<h1>Prescriptions</h1>
+<ul>
+<li><a href="patientsPresciption.php">View prescriptions for patient</a></li>
+<li><a href="prescription.php">Input new prescription</a></li>
+</ul>
+<h1>Employees</h1>
+<ul>
+<li><a href="employees.php">View employees</a></li>
+<li><a href="employee.php">View employee information</a></li>
+<li><a href="addEmployee.php">Add an employee</a></li>
+<li><a href="editEmployee.php">Edit an employee's information</a></li>
+</ul>
+</body>
+</html>
