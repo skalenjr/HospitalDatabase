161f17d42062272f4970fa687c7f24d6dd047d14
kalensn <kalensn@mymail.vcu.edu>
2020-04-30 23:29:04 -0400

    Fixed formatting error

--- HospitalDatabase/Patients/editPatient.php
+++ HospitalDatabase/Patients/editPatient.php
@@ -20,7 +20,7 @@ if (!isset($_GET['pID']) && $_SERVER['REQUEST_METHOD'] != 'POST')
     echo "</select>";
     echo "</form>";
 }
-else if(isset($_SERVER['REQUEST_METHOD'] != 'POST'){
+else if($_SERVER['REQUEST_METHOD'] != 'POST'){
     //show current pateint information in form
     $stmt = $conn->prepare("select Patient.pID, Patient.SSN, Person.first_name, Person.last_name, Patient.type_of_insurance from Person, Patient where Person.SSN = Patient.SSN and Patient.pID = $_GET[pID]");
     $stmt->execute();
