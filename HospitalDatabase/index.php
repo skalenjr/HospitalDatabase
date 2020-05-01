956bdd519f387fd8347da0b8080c5f482e8a75ef
kalensn <kalensn@mymail.vcu.edu>
2020-05-01 02:39:31 -0400

    Fixed formatting

--- HospitalDatabase/index.php
+++ HospitalDatabase/index.php
@@ -20,7 +20,6 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 else{
     
     try{
-        
         $_SESSION["hashedPassword"] = password_hash($_POST['password'], PASSWORD_BCRYPT);
         echo password_hash('password', PASSWORD_BCRYPT);
         
