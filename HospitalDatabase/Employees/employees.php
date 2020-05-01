efd3a0f7658ae9c850dbad76c16143d5dba3bb79
kalensn <kalensn@mymail.vcu.edu>
2020-05-01 18:42:45 -0400

    Added Employees.php functionallity

--- HospitalDatabase/Employees/employees.php
+++ HospitalDatabase/Employees/employees.php
@@ -1,1 +1,22 @@
 <?php
+
+session_start();
+
+require_once('../connection.php');
+
+echo "<h1><a href='../hospitaldatabase.php'>Hospital Database</a></h1>";
+echo "<h2>Employees</h2>";
+
+$stmt = $conn->prepare("select Employee.eID, Person.first_name, Person.last_name from Employee, Person where Employee.SSN=Person.SSN;");
+$stmt->execute();
+
+echo "<table style='border: solid 1px black;'>";
+echo "<thead><tr><th>Employee ID</th><th>First name</th><th>Last name</th></tr></thead>";
+echo "<tbody>";
+while ($row = $stmt->fetch()) {
+    echo "<tr><td><a href='patient.php?eID=$row[eID]'>$row[eID]</a></td><td>$row[first_name]</td><td>$row[last_name]</td></tr>";
+}
+echo "</tbody>";
+echo "</table>";
+
+?>
