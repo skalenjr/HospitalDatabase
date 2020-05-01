500e14c4cc0e23f3ac726868ca56f214a211f664
kalensn <kalensn@mymail.vcu.edu>
2020-05-01 01:07:37 -0400

    Fixed formatting

--- HospitalDatabase/Patients/addPatient.php
+++ HospitalDatabase/Patients/addPatient.php
@@ -32,7 +32,7 @@ else{
         echo "Error: " . $e->getMessage();
     }
     echo "Patient Succesfully Added<br/>";
-    $stmt = $conn->prepare("select pID from patients where SSN= $_POST[SSN];");
+    $stmt = $conn->prepare("select pID from Patients where SSN= $_POST[SSN];");
     $stmt->execute();
     $row = $stmt->fetch();
     echo "<a href='patient.php?pID=$row[pID]'>View patient's information</a>";
