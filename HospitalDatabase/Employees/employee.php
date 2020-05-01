8536713e1d0e3993031aa776af30fc58610d12da
kalensn <kalensn@mymail.vcu.edu>
2020-05-01 19:44:28 -0400

    Added address displays

--- HospitalDatabase/Employees/employee.php
+++ HospitalDatabase/Employees/employee.php
@@ -10,14 +10,14 @@ echo "<h2>Employee Information</h2>";
 if (isset($_GET['eID']))
 {
     //give employee's information
-    $stmt = $conn->prepare("select Employee.eID, Employee.SSN, Person.first_name, Person.last_name, Employee.hire_date from Person, Employee where Person.SSN = Employee.SSN and Employee.eID = $_GET[eID]");
+    $stmt = $conn->prepare("select Employee.eID, Employee.SSN, Person.first_name, Person.last_name, Person.address, Employee.hire_date from Person, Employee where Person.SSN = Employee.SSN and Employee.eID = $_GET[eID]");
     $stmt->execute();
     
     echo "<table style='border: solid 1px black;'>";
-    echo "<thead><tr><th>Employee ID</th><th>SSN</th><th>First name</th><th>Last name</th><th>Hire Date</th></tr></thead>";
+    echo "<thead><tr><th>Employee ID</th><th>SSN</th><th>First name</th><th>Last name</th><th>Address</th><th>Hire Date</th></tr></thead>";
     echo "<tbody>";
     while ($row = $stmt->fetch()) {
-        echo "<tr><td>$row[eID]<td>$row[SSN]</td></td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[hire_date]</td></tr>";
+        echo "<tr><td>$row[eID]<td>$row[SSN]</td></td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[address]</td><td>$row[hire_date]</td></tr>";
         $eID = $row['eID'];
     }
     echo "</tbody>";
--- HospitalDatabase/Patients/patient.php
+++ HospitalDatabase/Patients/patient.php
@@ -10,14 +10,14 @@ echo "<h2>Patient Information</h2>";
 if (isset($_GET['pID']))
 {
     //give patients information
-    $stmt = $conn->prepare("select Patient.pID, Patient.SSN, Person.first_name, Person.last_name, Patient.type_of_insurance from Person, Patient where Person.SSN = Patient.SSN and Patient.pID = $_GET[pID]");
+    $stmt = $conn->prepare("select Patient.pID, Patient.SSN, Person.first_name, Person.last_name, Person.address, Patient.type_of_insurance from Person, Patient where Person.SSN = Patient.SSN and Patient.pID = $_GET[pID]");
     $stmt->execute();
     
     echo "<table style='border: solid 1px black;'>";
-    echo "<thead><tr><th>Patient ID</th><th>SSN</th><th>First name</th><th>Last name</th><th>Type Of Insurance</th></tr></thead>";
+    echo "<thead><tr><th>Patient ID</th><th>SSN</th><th>First name</th><th>Last name</th><th>Type Of Insurance</th><th>Address</th></tr></thead>";
     echo "<tbody>";
     while ($row = $stmt->fetch()) {
-        echo "<tr><td>$row[pID]<td>$row[SSN]</td></td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[type_of_insurance]</td></tr>";
+        echo "<tr><td>$row[pID]<td>$row[SSN]</td></td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[type_of_insurance]</td></tr><td>$row[address]</td>";
         $pID = $row['pID'];
     }
     echo "</tbody>";
