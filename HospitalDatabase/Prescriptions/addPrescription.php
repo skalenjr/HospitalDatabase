bc0449e0bd745ef672c50e33b2928153bebcc343
kalensn <kalensn@mymail.vcu.edu>
2020-05-02 02:30:55 -0400

    Fixed formatting

--- HospitalDatabase/Prescriptions/addPrescription.php
+++ HospitalDatabase/Prescriptions/addPrescription.php
@@ -2,7 +2,7 @@
 
 ​session_start();
 
-​require_once('../connection.php');
+require_once('../connection.php');
 
 echo "<h1><a href='../hospitaldatabase.php'>Hospital Database</a></h1>";
 echo "<h2>Add A Prescription</h2>";
