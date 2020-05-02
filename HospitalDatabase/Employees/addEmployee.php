a9230a48ec3fef13921fe2a9068d8597f0ef3fed
kalensn <kalensn@mymail.vcu.edu>
2020-05-01 21:03:36 -0400

    Fixed formatting

--- HospitalDatabase/Employees/addEmployee.php
+++ HospitalDatabase/Employees/addEmployee.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 else{
     try {
         $stmt = $conn->prepare("INSERT IGNORE INTO Person (first_name, last_name, SSN, address, DOB) VALUES(:first_name, :last_name, :SSN, :Address, :DOB);
-        INSERT IGNORE INTO Employee(SSN) VALUES(:SSN);");
+        INSERT IGNORE INTO Employee(SSN, hire_date) VALUES(:SSN, CURDATE());");
         $stmt->bindValue(':first_name', $_POST['first_name']);
         $stmt->bindValue(':last_name', $_POST['last_name']);
         $stmt->bindValue(':SSN', $_POST['SSN']);
