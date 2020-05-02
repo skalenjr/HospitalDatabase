9ff724ff1ff054eb41979bbbae8e3b60396c9e39
kalensn <kalensn@mymail.vcu.edu>
2020-05-01 20:06:18 -0400

    Fixed formatting

--- HospitalDatabase/Patients/patient.php
+++ HospitalDatabase/Patients/patient.php
@@ -14,7 +14,7 @@ if (isset($_GET['pID']))
     $stmt->execute();
     
     echo "<table style='border: solid 1px black;'>";
-    echo "<thead><tr><th>Patient ID</th><th>SSN</th><th>First name</th><th>Last name</th><th>Type Of Insurance</th><th>Address</th></tr></thead>";
+    echo "<thead><tr><th>Patient ID</th><th width=150px>SSN</th><th>First name</th><th>Last name</th><th width=150px>Type Of Insurance</th><th width=400px>Address</th></tr></thead>";
     echo "<tbody>";
     while ($row = $stmt->fetch()) {
         echo "<tr><td>$row[pID]<td>$row[SSN]</td></td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[type_of_insurance]</td></tr><td>$row[address]</td>";
