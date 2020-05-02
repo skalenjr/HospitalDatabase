ac04aa7174deac5e925b21a0efff04985ddaaeee
kalensn <kalensn@mymail.vcu.edu>
2020-05-02 01:35:29 -0400

    Added procedure.php functionallity

--- HospitalDatabase/Procedures/procedures.php
+++ HospitalDatabase/Procedures/procedures.php
@@ -1,1 +1,22 @@
 <?php
+
+session_start();
+
+require_once('../connection.php');
+
+echo "<h1><a href='../hospitaldatabase.php'>Hospital Database</a></h1>";
+echo "<h2>Procedures</h2>";
+
+$stmt = $conn->prepare("select Procedure.procedureID, Person.first_name, Person.last_name,  from Person, Procedure where Procedure.pID = Patient.pID order by Procedure.procedureID");
+$stmt->execute();
+
+echo "<table style='border: solid 1px black;'>";
+echo "<thead><tr><th>Procedure ID</th><th>First name</th><th>Last name</th></tr></thead>";
+echo "<tbody>";
+while ($row = $stmt->fetch()) {
+    echo "<tr><td><a href='patient.php?pID=$row[pID]'>$row[pID]</a></td><td>$row[first_name]</td><td>$row[last_name]</td></tr>";
+}
+echo "</tbody>";
+echo "</table>";
+
+?>
