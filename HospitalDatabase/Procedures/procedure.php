82efba85c90e59037b2c29e53519c20fb2a0991d
kalensn <kalensn@mymail.vcu.edu>
2020-05-02 02:45:51 -0400

    Fixed formatting

--- HospitalDatabase/Procedures/procedure.php
+++ HospitalDatabase/Procedures/procedure.php
@@ -18,28 +18,52 @@ if (isset($_GET['procID']))
     echo "<thead><tr><th>Procedure ID</th><th>Procedure Name</th><th>First name</th><th>Last name</th><th>Procedure Cost</th><th>Room Number</th></tr></thead>";
     echo "<tbody>";
     while ($row = $stmt->fetch()) {
-        echo "<tr><td>$row[procID]<td>$row[procedure_name]</td></td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[cost]</td><td>$row[room_number]</td></tr>";
+        echo "<tr><td>$row[procID]</td><td>$row[procedure_name]</td><td>$row[first_name]</td><td>$row[last_name]</td><td>$row[cost]</td><td>$row[room_number]</td></tr>";
         $pID = $row['pID'];
     }
     echo "</tbody>";
     echo "</table>";
+    echo "<a href='patientsProcedures.php?pID=$pID'>View all patient's procedures</a><br/>";
+    echo "<a href='../patient.php?pID=$pID'>View patient's information</a><br/><br/>";
     
     $stmt = $conn->prepare("select Person.first_name, Person.last_name from Person, Procedures, Procedure_Docs, Employee where Procedures.procID = $_GET[procID] and Procedures.procID = Procedure_Docs.procID and
     Procedure_Docs.doctor = Employee.eID and Person.SSN=Employee.SSN");
     $stmt->execute();
     
-    echo "<table width=900px style='border: solid 1px black;'>";
+    echo "<table style='border: solid 1px black;'>";
     echo "<thead><tr><th>Doctor's First Name</th><th>Doctor's Last Name</th></thead>";
     echo "<tbody>";
     while ($row = $stmt->fetch()) {
-        echo "<tr><td>$row[first_name]<td>$row[last_name]</td></td";
-        $pID = $row['pID'];
+        echo "<tr><td>$row[first_name]</td><td>$row[last_name]</td></tr>";
     }
     echo "</tbody>";
     echo "</table>";
     
-    echo "<a href='patientsProcedures.php?pID=$pID'>View all patient's procedures</a><br/>";
-    echo "<a href='../patient.php?pID=$pID'>View patient's information</a><br/><br/>";
+    $stmt = $conn->prepare("select Person.first_name, Person.last_name from Person, Procedures, Procedure_Nurses, Employee where Procedures.procID = $_GET[procID] and Procedures.procID = Procedure_Nurses.procID and
+    Procedure_Nurses.nurses = Employee.eID and Person.SSN=Employee.SSN");
+    $stmt->execute();
+    
+    echo "<table style='border: solid 1px black;'>";
+    echo "<thead><tr><th>Nurse's First Name</th><th>Nurse's Last Name</th></thead>";
+    echo "<tbody>";
+    while ($row = $stmt->fetch()) {
+        echo "<tr><td>$row[first_name]</td><td>$row[last_name]</td></tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+    
+    $stmt = $conn->prepare("select Procedure_Med.medication from Person, Procedures, Procedure_Nurses, Employee where Procedures.procID = $_GET[procID] and Procedures.procID = Procedure_Med.procID");
+    $stmt->execute();
+    
+    echo "<table style='border: solid 1px black;'>";
+    echo "<thead><tr><th>Medication Used</th></thead>";
+    echo "<tbody>";
+    while ($row = $stmt->fetch()) {
+        echo "<tr><td>$row[medication]</td></tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+    
     unset($pID);
 }
 else {
