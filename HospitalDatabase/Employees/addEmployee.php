678a9a571ec98790f63c46cb81db1a3c7e2f1b1b
kalensn <kalensn@mymail.vcu.edu>
2020-05-01 23:25:50 -0400

    Fixed formatting

--- HospitalDatabase/Employees/addEmployee.php
+++ HospitalDatabase/Employees/addEmployee.php
@@ -38,7 +38,7 @@ else{
     try {
         $_SESSION['SSN'] = $_POST['SSN'];
         $stmt = $conn->prepare("INSERT IGNORE INTO Person (first_name, last_name, SSN, address, DOB) VALUES(:first_name, :last_name, :SSN, :address, :DOB);
-        INSERT IGNORE INTO Employee(SSN, hire_date, salary, department_ID, job_title) VALUES(:SSN, CURDATE(), :salary, :department_ID, :job_title);");
+        INSERT IGNORE INTO Employee(hire_date, salary, department_ID, job_title, SSN) VALUES(CURDATE(), :salary, :department_ID, :job_title, :SSN);");
         $stmt->bindValue(':first_name', $_POST['first_name']);
         $stmt->bindValue(':last_name', $_POST['last_name']);
         $stmt->bindValue(':SSN', $_SESSION['SSN']);
@@ -56,10 +56,12 @@ else{
         echo "Error: " . $e->getMessage();
     }
     echo "Employee Succesfully Added<br/>";
-    $stmt = $conn->prepare("select eID from Employee where SSN= $_SESSION[SSN];");
+    $SSN = $_SESSION['SSN'];
+    $stmt = $conn->prepare("select eID from Employee where SSN= $SSN;");
     $stmt->execute();
     $row = $stmt->fetch();
     echo "<a href='employee.php?eID=$row[eID]'>View employee's information</a><br/>";
     unset($_SESSION['SSN']);
+    unset($SSN);
 }
 ?><?php
