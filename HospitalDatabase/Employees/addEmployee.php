60153a0e48aea0339a2a19818eaefdb5c0340d18
kalensn <kalensn@mymail.vcu.edu>
2020-05-01 23:08:47 -0400

    Fixed formatting

--- HospitalDatabase/Employees/addEmployee.php
+++ HospitalDatabase/Employees/addEmployee.php
@@ -38,7 +38,7 @@ else{
     try {
         $_SESSION['SSN'] = $_POST['SSN'];
         $stmt = $conn->prepare("INSERT IGNORE INTO Person (first_name, last_name, SSN, address, DOB) VALUES(:first_name, :last_name, :SSN, :address, :DOB);
-        INSERT IGNORE INTO Employee(SSN, hire_date, salary, department_ID, job_title) VALUES(:SSN, CURDATE(), :salary, :department_ID, :job_title);");
+        INSERT INTO Employee(SSN, hire_date, salary, department_ID, job_title) VALUES(:SSN, CURDATE(), :salary, :department_ID, :job_title);");
         $stmt->bindValue(':first_name', $_POST['first_name']);
         $stmt->bindValue(':last_name', $_POST['last_name']);
         $stmt->bindValue(':SSN', $_SESSION['SSN']);
