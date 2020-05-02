3f0367930621cb3e5e258e129788e81a31a8017e
kalensn <kalensn@mymail.vcu.edu>
2020-05-02 00:19:59 -0400

    Fixed formatting

--- HospitalDatabase/Employees/addEmployee.php
+++ HospitalDatabase/Employees/addEmployee.php
@@ -41,10 +41,11 @@ else{
             INSERT IGNORE INTO Employee (SSN, hire_date, salary, department_ID, job_title) VALUES(:SSN, CURDATE(), :salary, :department_ID, :job_title);"
          */
         $stmt = $conn->prepare("INSERT IGNORE INTO Person (first_name, last_name, SSN, address, DOB) VALUES(:first_name, :last_name, :SSN, :Address, :DOB);
-        INSERT IGNORE INTO Employee(SSN, hire_date, salary, department_ID, job_title) VALUES(:SSN, :hire_date, :salary, :department_ID, :job_title);");
+        INSERT IGNORE INTO Employee(SSN, hire_date, salary, department_ID, job_title) VALUES(:SSN2, :hire_date, :salary, :department_ID, :job_title);");
         $stmt->bindValue(':first_name', $_POST['first_name']);
         $stmt->bindValue(':last_name', $_POST['last_name']);
         $stmt->bindValue(':SSN', $_POST['SSN']);
+        $stmt->bindValue(':SSN2', $_POST['SSN']);
         $stmt->bindValue(':address', $_POST['address']);
         $stmt->bindValue(':DOB', $_POST['DOB']);
         $stmt->bindValue(':salary', $_POST['salary']);
