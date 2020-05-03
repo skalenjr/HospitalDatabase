f1ebfb2371be24c045bdc4c77693ffb197ce508e
kalensn <kalensn@mymail.vcu.edu>
2020-05-03 00:54:02 -0400

    Fixed formatting

--- HospitalDatabase/index.php
+++ HospitalDatabase/index.php
@@ -23,8 +23,6 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 else{
     
     try{
-        $_SESSION["hashedPassword"] = password_hash($_POST['password'], PASSWORD_BCRYPT);
-        
         $STMT = $conn->prepare("select username from login_info where username = :username");
         $STMT->bindValue(':username', $_POST['username']);
         $STMT->execute();
@@ -36,25 +34,21 @@ else{
             $STMT->execute();           
             $row = $STMT-> fetch();
             if(password_verify($_POST['password'], $row[password])) {        
-                unset ($_SESSION["hashedPassword"]);
                 $_SESSION['loggedin']=True;
                 header("Location: hospitaldatabase.php");
             } 
             else{
-                unset ($_SESSION["hashedPassword"]);
                 echo "Password Incorrect";
                 echo "<br /><a href='index.php'>Try again</a>";
                 
             }
         }
         else{
-            unset ($_SESSION["hashedPassword"]);
             echo "Username Incorrect";
             echo "<br /><a href='index.php'>Try again</a>";
         }
         
     } catch (PDOException $e) {
-        unset ($_SESSION["hashedPassword"]);
         echo "Error: " . $e->getMessage();
     }
 }
