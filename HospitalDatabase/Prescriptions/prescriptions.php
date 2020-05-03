4edaacd4061d6a00e3927700e036a6698fb243e4
kalensn <kalensn@mymail.vcu.edu>
2020-05-02 23:39:04 -0400

    Fixed formatting

--- HospitalDatabase/Prescriptions/prescriptions.php
+++ HospitalDatabase/Prescriptions/prescriptions.php
@@ -7,7 +7,7 @@ require_once('../connection.php');
 echo "<h1><a href='../hospitaldatabase.php'>Hospital Database</a></h1>";
 echo "<h2>Prescriptions</h2>";
 
-$stmt = $conn->prepare("select prescriptionID, pID, visitID, medication, directions, start_date, end_date from Prescription Person, Patient where Person.SSN = Patient.SSN order by Patient.pID");
+$stmt = $conn->prepare("select prescriptionID, Prescription.pID, visitID, medication, directions, start_date, end_date from Prescription Person, Patient where Person.SSN = Patient.SSN order by Patient.pID");
 $stmt->execute();
 
 echo "<table style='border: solid 1px black;'>";
