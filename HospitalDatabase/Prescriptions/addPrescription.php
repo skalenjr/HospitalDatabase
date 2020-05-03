44b0eacfc28784879da9714944bb1e4f399babc1
kalensn <kalensn@mymail.vcu.edu>
2020-05-03 03:47:59 -0400

    Fixed formatting

--- HospitalDatabase/Prescriptions/addPrescription.php
+++ HospitalDatabase/Prescriptions/addPrescription.php
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
 }
 else{
     try {
-        $stmt = $conn->prepare("INSERT IGNORE INTO Prescription (pID, visitID, medication , directions, start_date, end_date) VALUES(:pID, :visitID, :medication, :directions, :start_date, :end_date);");
+        $stmt = $conn->prepare("INSERT IGNORE INTO Prescription (pID, visitID, medication, directions, start_date, end_date) VALUES(:pID, :visitID, :medication, :directions, :start_date, :end_date);");
         $stmt->bindValue(':pID', $_POST['pID']);
         $stmt->bindValue(':visitID', $_POST['visitID']);
         $stmt->bindValue(':medication', $_POST['medication_name']);
